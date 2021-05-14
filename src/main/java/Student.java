0c7c7d706ea352e470d13fc3aebb877aa9521080
shanshan-su <78042275+shanshan-su@users.noreply.github.com>
2021-05-14 12:19:24 -0500

    feat: completed grades and addGrade tests

--- src/main/java/Student.java
+++ src/main/java/Student.java
@@ -1,20 +1,34 @@
+import java.util.ArrayList;
+
 public class Student {
 
     private long id;
     private String name;
+    private ArrayList<Integer> grades;
 
     public Student(long id, String name) {
         this.id = id;
         this.name = name;
+        this.grades = new ArrayList<>();
     }
 
+    // returns the student's id
     public long getId() {
         return id;
     }
 
+    // returns the student's name
     public String getName() {
         return name;
     }
 
+    // returns the list of grades
+    public ArrayList<Integer> getGrades(){
+        return grades;
+    }
 
+    // adds the given grade to the grades list
+    public void addGrade(int grade) {
+        this.grades.add(grade);
+    }
 }
--- src/test/java/StudentTest.java
+++ src/test/java/StudentTest.java
@@ -1,4 +1,7 @@
+import org.junit.Before;
 import org.junit.Test;
+
+import java.util.ArrayList;
 
 import static org.junit.Assert.*;
 
@@ -17,6 +20,26 @@ public class StudentTest {
         assertNotEquals("test1", test.getName());
     }
 
+    @Test
+    public void testIfGradesIsInitialized() {
+        assertNotNull(test.getGrades());
+    }
 
+    @Test
+    public void testIfGradeCanBeAdded() {
+        assertEquals(0, test.getGrades().size());
+
+        test.addGrade(90);
+        assertEquals(1, test.getGrades().size());
+        assertNotEquals(2, test.getGrades().size());
+        assertSame(90, test.getGrades().get(0));
+        assertNotSame(99, test.getGrades().get(0));
+
+        test.addGrade(99);
+        assertEquals(2, test.getGrades().size());
+        assertNotEquals(1, test.getGrades().size());
+        assertSame(99, test.getGrades().get(1));
+        assertNotSame(90, test.getGrades().get(1));
+    }
 
 }
